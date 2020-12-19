fbb30269da1296de1c98f25640e55af7a7395e6f
xoancg <xoancastrogarcia@gmail.com>
2020-12-19 14:25:45 +0100

    Update!

--- es_primo.py
+++ es_primo.py
@@ -1,10 +1,16 @@
 number = int(input('Type a number: '))
 
-isprime = True
+if number > 1:
+    isprime = True
+    divisor = 2
 
-for divisor in range(2, number):
-    if number % divisor == 0:
-        isprime = False
+    while divisor < number:
+        if number % divisor == 0:
+            isprime = False
+            break
+        divisor += 1
+else:
+    isprime = False
 
 if isprime:
     print('{0} is a prime number! ;)'.format(number))
---
+++ es_primo_for.py
@@ -0,0 +1,12 @@
+number = int(input('Type a number: '))
+
+isprime = True
+
+for divisor in range(2, number):
+    if number % divisor == 0:
+        isprime = False
+
+if isprime:
+    print('{0} is a prime number! ;)'.format(number))
+else:
+    print('{0} is not a prime number! :('.format(number))
