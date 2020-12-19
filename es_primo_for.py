8344ca7d00e6fd37d1ddaa57327fb5bea69235b9
xoancg <xoancastrogarcia@gmail.com>
2020-12-19 14:30:14 +0100

    Update!

--- es_primo_for.py
+++ es_primo_for.py
@@ -5,6 +5,7 @@ isprime = True
 for divisor in range(2, number):
     if number % divisor == 0:
         isprime = False
+        break
 
 if isprime:
     print('{0} is a prime number! ;)'.format(number))
