124501213d75b163a35268078bfa23b63b9f6db0
xoancg <xoancastrogarcia@gmail.com>
2020-12-19 17:44:40 +0100

    inversion.py

--- inversion.py
+++ inversion.py
@@ -7,4 +7,7 @@ for i in string:
 
 print('Reverse is: ', reverse)
 
-# What the code does is to read the characters of the string one by one and save them in the variable 'reverse'. Each new character is saved to the left of the previously saved one.
+'''
+What the code does is to read the characters of the string one by one and save them in the variable 'reverse'.
+Each new character is saved to the left of the previously saved one.
+'''
